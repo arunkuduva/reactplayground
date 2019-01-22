e810ff9dfb8236f3a90b012a3a1edc5b03b192c4
arunkuduva@gmail.com <arunkuduva@gmail.com>
2019-01-22 00:29:40 -0600

    second

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,7 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 class Counter extends Component {
 
     state = {
-        count:0,
+        count:1,
+        tags: ['tag1', 'tag2', 'tag3'],
         imageUrl : 'https://picsum.photos/200'
     };
 
@@ -12,11 +13,18 @@ class Counter extends Component {
        return (
        <div>
             <img src = {this.state.imageUrl} alt = ""/>
-            <span className = "badge badge-primary m-2"> 
+            <span className = {this.getBadgeClasses()}> 
             {this.formatCount()}</span>
            <button className = "btn btn-secondary">Increment</button>
+           <ul >{this.state.tags.map(tag => <li key = {tag}> { tag }</li>)}</ul>
        </div>
        ) ; 
+       }
+       getBadgeClasses()
+       {
+           let classes = "badge m-2 badge-";
+           classes += this.state.count === 0 ? "warning" : "primary";
+           return classes;
        }
        formatCount()
        {
